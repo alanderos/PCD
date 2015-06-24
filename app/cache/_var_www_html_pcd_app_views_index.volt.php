7ac35c9d5db8095f6e18878e39c3ea3b2740858b
alx <alx@alx-K53TA>
2015-06-23 19:40:03 -0500

    primer commit con login

---
+++ app/cache/_var_www_html_pcd_app_views_index.volt.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">-->
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Phalcon PHP Framework</title>
+        <base href="/PDC/"/>
+    
+        <?php echo $this->tag->stylesheetLink('css/cssPlantilla.css'); ?>
+    
+    </head>
+    <body>
+        <?php echo $this->getContent(); ?>
+    </body>
+</html>
---
+++ app/views/index/index.volt
@@ -0,0 +1,12 @@
+{{ stylesheet_link('css/cssLogin.css') }}
+<div id="cuadrito">
+    <div>
+        <span id=titulo>Iniciar sesion</span>
+    </div>
+    <form>
+        <input name="email" type="email placeholder="email"/>
+        <input name="contrasena" type="password"/>
+        <input type="submit"/>
+    </form>
+    <a ><div id="registroBoton">REGISTRATE</div></a>
+</div>
